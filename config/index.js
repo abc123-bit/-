0965ddaa205b72135d91b65733e1068f4039cbf0
abc123-bit <76656440+abc123-bit@users.noreply.github.com>
2022-08-30 18:29:26 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wx085d7f62d80a4890",
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "aa9184227312a0173ab7243feb7b7249",
     // 模板消息id
-    TEMPLATE_ID: "",
+    TEMPLATE_ID: "0C_8vQ0jjRHaKdAOCwxHgT7LrGfP3hy0tbzllaWz5sA",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    USERS: [""],
+    USERS: ["oO95X6xNiVFJiS0JNArFcXZhswEA"],
      
     /**
      * 信息配置
@@ -19,9 +19,9 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "广西",
     // 所在城市
-    CITY: "惠州",
+    CITY: "梧州",
 
     /** 重要节日相关 */
 
